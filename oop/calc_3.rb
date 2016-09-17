26f8cf29e229c312eaca6f9c132a2b65872636ad
Joy <joymccaffreychicago@gmail.com>
2016-09-17 16:39:23 -0500

    Complete assignment and pass tests.

--- oop/calc_3.rb
+++ oop/calc_3.rb
@@ -4,8 +4,7 @@
 # DRY up all the code below - there shouldn't be a single method duplicated between
 # any two classes.
 
-class SimpleCalculator
-
+module Math
   def add(first_number, second_number)
     first_number + second_number
   end
@@ -20,55 +19,23 @@ class SimpleCalculator
 
   def divide(first_number, second_number)
     first_number / second_number
-  end
+  end 
+end
 
+class SimpleCalculator
+  include Math
 end
 
 class FancyCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
+  include Math
 
   def square_root(number)
     Math.sqrt(number)
   end
-
 end
 
-class WhizBangCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
-
-  def square_root(number)
-    Math.sqrt(number)
-  end
+class WhizBangCalculator < FancyCalculator
+  include Math
 
   def hypotenuse(first_number, second_number)
     Math.hypot(first_number, second_number)
@@ -79,8 +46,58 @@ class WhizBangCalculator
     exponent_number.times { total = multiply(total,first_number) }
     total
   end
-
 end
 
 # Copy your driver code from the previous exercise and more below:
+simple_calc = SimpleCalculator.new
+fancy_calc = FancyCalculator.new
+whizbang_calc = WhizBangCalculator.new
 
+puts "Testing simple calculator using a module using the add method...."
+result = simple_calc.add(1, 2)
+
+if result == 3
+  puts "PASS!"
+else
+  puts "FAIL!"
+end
+
+puts "-------------"
+puts "Testing fancy calculator using a module using the add method..."
+result = fancy_calc.add(1, 2)
+
+if result == 3
+  puts "PASS!"
+else
+  puts "FAIL!"
+end
+
+puts "-------------"
+puts "Using the fancy calculator to find the square root of 4..."
+result = fancy_calc.square_root(4)
+
+if result == 2
+  puts "PASS!"
+else
+  puts "FAIL!"
+end
+
+puts "-------------"
+puts "Using the whizbang calculator to find the square root of 4..."
+result = whizbang_calc.add(1, 2)
+
+if result == 3
+  puts "PASS!"
+else
+  puts "FAIL!"
+end
+
+puts "-------------"
+puts "Using the whizbang calculator to find a hypotenuse..."
+result = whizbang_calc.hypotenuse(1, 2)
+
+if result == 2.23606797749979
+  puts "PASS!"
+else
+  puts "FAIL!"
+end
