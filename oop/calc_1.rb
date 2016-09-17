d47cc942d2a4d3eb382d31405dcaa9104954d853
Joy <joymccaffreychicago@gmail.com>
2016-09-17 16:36:10 -0500

    Complete exercise and pass tests.

--- oop/calc_1.rb
+++ oop/calc_1.rb
@@ -13,7 +13,6 @@
 # below. You must also write the driver code to test your classes.
 
 class SimpleCalculator
-
   def add(first_number, second_number)
     first_number + second_number
   end
@@ -29,32 +28,43 @@ class SimpleCalculator
   def divide(first_number, second_number)
     first_number / second_number
   end
-
 end
 
-class FancyCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
-
+class FancyCalculator < SimpleCalculator
   def square_root(number)
     Math.sqrt(number)
   end
-
 end
 
 # Write your own driver code below:
+simple_calc = SimpleCalculator.new
+fancy_calc = FancyCalculator.new
 
+puts "Testing simple calculator using the add method...."
+result = simple_calc.add(1, 2)
+
+if result == 3
+  puts "PASS!"
+else
+  puts "FAIL!"
+end
+
+puts "-------------"
+puts "Testing fancy calculator by inheriting from the simple calucator using the add method..."
+result = fancy_calc.add(1, 2)
+
+if result == 3
+  puts "PASS!"
+else
+  puts "FAIL!"
+end
+
+puts "-------------"
+puts "Using the fancy calculator to find the square root of 4..."
+result = fancy_calc.square_root(4)
+
+if result == 2
+  puts "PASS!"
+else
+  puts "FAIL!"
+end
