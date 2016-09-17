f9f38e197c44c2223552680b0b8b58fc664b06ba
Joy <joymccaffreychicago@gmail.com>
2016-09-17 16:37:54 -0500

    Complete assignment and pass tests.

--- oop/calc_2.rb
+++ oop/calc_2.rb
@@ -3,8 +3,7 @@
 # a different technique using Composition. Hint: Google 'Ruby Mixin Module'. (Using
 # mixin modules is how you achieve Composition with Ruby.)
 
-class SimpleCalculator
-
+module Math
   def add(first_number, second_number)
     first_number + second_number
   end
@@ -19,33 +18,49 @@ class SimpleCalculator
 
   def divide(first_number, second_number)
     first_number / second_number
-  end
+  end 
+end
 
+class SimpleCalculator
+  include Math
 end
 
 class FancyCalculator
-
-  def add(first_number, second_number)
-    first_number + second_number
-  end
-
-  def subtract(first_number, second_number)
-    first_number - second_number
-  end
-
-  def multiply(first_number, second_number)
-    first_number * second_number
-  end
-
-  def divide(first_number, second_number)
-    first_number / second_number
-  end
-
+  include Math
   def square_root(number)
     Math.sqrt(number)
   end
-
 end
 
 # Copy your driver code from the previous exercise below:
+simple_calc = SimpleCalculator.new
+fancy_calc = FancyCalculator.new
 
+puts "Testing simple calculator using a module using the add method...."
+result = simple_calc.add(1, 2)
+
+if result == 3
+  puts "PASS!"
+else
+  puts "FAIL!"
+end
+
+puts "-------------"
+puts "Testing fancy calculator using a module using the add method..."
+result = fancy_calc.add(1, 2)
+
+if result == 3
+  puts "PASS!"
+else
+  puts "FAIL!"
+end
+
+puts "-------------"
+puts "Using the fancy calculator to find the square root of 4..."
+result = fancy_calc.square_root(4)
+
+if result == 2
+  puts "PASS!"
+else
+  puts "FAIL!"
+end
